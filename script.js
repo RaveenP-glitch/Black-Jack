501035e05d2be09e47516876ce66a2f6a1be3fb3
Raveen Panditha <raveenpanditha8@gmail.com>
2021-03-05 17:16:38 +0530

    new lines

--- script.js
+++ script.js
@@ -8,12 +8,35 @@ let blackjackGame = {
 const YOU = blackjackGame['you']
 const DEALER = blackjackGame['dealer']
 
+const hitSound = new Audio('sounds/swish.m4a');
+
 document.querySelector('#hit-button').addEventListener('click', blackjackHit);
 
 function blackjackHit() {
+   showCard(YOU);
+   showCard(DEALER);
+
+}
+
+function showCard(activePlayer){
     let cardImage = document.createElement('img');
     cardImage.src = 'images/Q.png';
-    document.querySelector(YOU['div']).appendChild(cardImage);
-    
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelector('img');
+
+    for(i=0;i<yourImages.length;i++){
+        yourImages[i].remove();
+
+    }
+
+    for(i=0;i<dealerImages.length;i++){
+        dealerImages[i].remove();
+
+    }
 
 }
